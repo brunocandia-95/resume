a3cff91b20694a5c0c3554c7882bb07c0625ebff
Bruno de Candia <brunodecandia@gmail.com>
2022-03-26 18:36:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let linkedin = 'https://www.linkedin.com/in/bruno-de-candia/'
+let endereco = 'https://www.google.com/maps/place/Nova+Hartz,+RS,+93890-000/@-29.5862141,-50.9137814,14z/data=!3m1!4b1!4m5!3m4!1s0x95193c65beeb9085:0x1e5de6f5497eaccf!8m2!3d-29.5846755!4d-50.903423'
+let gitHub = 'https://github.com/brunocandia-95'
+let whatsapp = 'https://wa.me/5551997958247'
+let site = ''
+let email = 'mailto:brunodecandia@gmail.com'
+
+
+
+
+
+function goTo(link){
+
+    //window.open(link)
+
+    //window.location.href = link;
+
+    window.open(email, 'emailWindow');
+    
+}
